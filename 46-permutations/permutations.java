d739f3e947d75b30b1fcf03b8df4a84bcf5fd63f
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-03-09 22:21:07 +0530

    Time: 1 ms (92.68%) | Memory: 44.5 MB (64.41%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> ans;
+    public List<List<Integer>> permute(int[] nums) {
+        ans=new ArrayList<>();
+        helper(nums,new ArrayList<Integer>());
+        return ans;
+
+    }
+    public void helper(int[] arr,List<Integer> used){
+        if(used.size() == arr.length){
+            ans.add(new ArrayList<>(used));
+            return;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(!used.contains(arr[i])){
+                used.add(arr[i]);
+                helper(arr,used);
+                used.removeLast();
+            }
+        }
+    }
+} 
