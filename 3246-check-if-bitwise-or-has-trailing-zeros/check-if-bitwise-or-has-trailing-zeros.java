50fbc7dbc62963fec2b82bc6140a18a0596c6988
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-03-15 01:31:03 +0530

    Time: 1 ms (56.96%) | Memory: 44 MB (85.57%) - LeetSync

--- 3246-check-if-bitwise-or-has-trailing-zeros/check-if-bitwise-or-has-trailing-zeros.java
+++ 3246-check-if-bitwise-or-has-trailing-zeros/check-if-bitwise-or-has-trailing-zeros.java
@@ -3,7 +3,6 @@ class Solution {
         int n=nums.length;
         int ans=0,idx=0;
         List<Integer> num=new ArrayList<>();
-        boolean even=false;
         for(int i=0;i<n;i++){
             if(nums[i] % 2 == 0){
                 num.add(nums[i]);
@@ -14,10 +13,9 @@ class Solution {
         }
          for(int i=0;i<num.size();i++){
             ans|=num.get(i);
-        }
-
-        if(((ans >> 0) & 1) == 0){
-            return true;
+            if(((ans >> 0) & 1) == 0){
+                return true;
+            }
         }
         return false;
         
