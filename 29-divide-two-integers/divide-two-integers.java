6d6b3312572195c3d6cc8ef2a35e40b4efbfd608
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-03-24 20:56:59 +0530

    Time: 1 ms (72.59%) | Memory: 40.6 MB (97.66%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+
+        long ans=0, sign=1;
+
+        if(dividend >= 0 && divisor < 0 || divisor >= 0 && dividend < 0) sign=-1;
+
+        long dd = Math.abs((long) dividend);
+        long dr = Math.abs((long) divisor);
+
+        for(int i=31;i>=0;i--){
+            if((dd >> i) >= dr){
+                dd-=(dr << i);
+                ans+= (1 << i);
+            }
+        }
+
+        return (int)(ans * sign);
+    }
+}
