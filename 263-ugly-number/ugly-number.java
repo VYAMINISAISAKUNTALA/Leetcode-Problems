cbf71472ce9d7245a9743f4b90acb2821c64ce02
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-03-31 22:23:43 +0530

    Time: 1 ms (57.22%) | Memory: 41.2 MB (14.45%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -15,30 +15,6 @@ class Solution {
             return false;
         }
         return true;
-        
-        // if(n == 1){
-        //     return true;
-        // }
-        // return primeFactors(n);
-    }
-
-    public boolean primeFactors(int n){
-        while(n % 2 == 0){
-            n/=2;
-        }
-        for(int i=3;i<=Math.sqrt(n);i+=2){
-            while(n % i == 0){
-                if(i > 5){
-                    return false;
-                }
-                n/=i;
-            }            
-        }
-        if(n > 5){
-            return false;
-        }
-        return true;
-
     }
 
 
