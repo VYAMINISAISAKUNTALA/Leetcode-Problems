bbf10665b63df9fcdaaa98320c4bf93bbafe3fca
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-03-31 22:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (23.18%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,10 +1,25 @@
 class Solution {
     public boolean isUgly(int n) {
-        if(n == 1){
-            return true;
-        }if(n <= 0) return false;
+
+        if(n <= 0) return false;
+        while(n % 2 == 0){
+            n/=2;
+        }
+        while(n % 3 == 0){
+            n/=3;
+        }
+        while(n % 5 == 0){
+            n/=5;
+        }
+        if(n > 5){
+            return false;
+        }
+        return true;
         
-        return primeFactors(n);
+        // if(n == 1){
+        //     return true;
+        // }
+        // return primeFactors(n);
     }
 
     public boolean primeFactors(int n){
