16a63c96d4893077f3ea7a5a78586ce97de70c98
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-04-06 20:01:53 +0530

    Time: 4 ms (13.02%) | Memory: 41.7 MB (82.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,8 +2,11 @@ class Solution {
     public boolean isValid(String s) {
         
         int len = s.length();
+
         if(len == 1) return false;
+
         Stack<Character> stk= new Stack();
+
         for(int i=0;i<len;i++){
             if(s.charAt(i) == ')' && !stk.isEmpty() && stk.peek() == '('){
                 stk.pop();
@@ -14,19 +17,6 @@ class Solution {
             }else{
                 stk.push(s.charAt(i));
             }
-            
-            // if(s.charAt(i) =='('){
-            //     stk.push('(');
-            // }else if(s.charAt(i) =='['){
-            //     stk.push('[');
-            // }else if(s.charAt(i) =='{'){
-            //     stk.push('{');
-            // }else if(s.charAt(i) ==')' && stk.contains('(')){
-            //     stk.removeElement('(');
-            // }else if(s.charAt(i) ==']' && stk.contains('[')){
-            //     stk.removeElement('[');
-            // }else if(s.charAt(i) == '}' && stk.contains('{')){
-            //     stk.removeElement('{');
 
         }
         return stk.size() == 0;
