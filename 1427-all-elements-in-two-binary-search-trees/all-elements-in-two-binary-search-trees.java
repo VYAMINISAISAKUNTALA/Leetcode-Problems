faf44b699b1c14ee1d5ef51e189a96861382a9ce
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-04-22 20:03:15 +0530

    Time: 13 ms (95.42%) | Memory: 46.6 MB (33.82%) - LeetSync

---
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public List<Integer> inOrderTraversal(TreeNode root){
+
+         List<Integer> resultList = new ArrayList<>();
+         inOrderHelper(root, resultList);
+         return resultList;
+
+    }
+
+    private void inOrderHelper(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+
+        inOrderHelper(node.left, list);
+        list.add(node.val);
+        inOrderHelper(node.right, list);
+    }
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+
+        List<Integer> resultList= new ArrayList<>();
+        List<Integer> list1= inOrderTraversal(root1);
+        List<Integer> list2 = inOrderTraversal(root2);
+
+        int list1Pointer = 0, list2Pointer=0;
+        int n1 = list1.size(), n2 = list2.size();
+
+        while(list1Pointer < n1  && list2Pointer < n2){
+            if(list1.get(list1Pointer) <= list2.get(list2Pointer)){
+                resultList.add(list1.get(list1Pointer));
+                list1Pointer++;
+            }else{
+                resultList.add(list2.get(list2Pointer));
+                list2Pointer++;
+            }
+        }
+        while(list1Pointer < n1){
+            resultList.add(list1.get(list1Pointer++));
+        }
+
+        while(list2Pointer < n2){
+            resultList.add(list2.get(list2Pointer++));
+        }
+        return resultList;
+
+        
+        
+    }
+}
