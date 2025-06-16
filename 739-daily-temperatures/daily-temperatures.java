dc289f9775e54681ea52eb0aef4a4d01e5c52438
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-06-16 19:49:44 +0530

    Time: 82 ms (16.54%) | Memory: 60.3 MB (65.76%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-
     
      int len = temperatures.length;
      if(len == 0) return new int[]{0};   
@@ -8,10 +7,7 @@ class Solution {
 
      int[] result = new int[len];
 
-     stk.push(len-1);
-     result[len-1] = 0;
-
-     for(int i = len-2;i>=0;i--){
+     for(int i = len-1;i>=0;i--){
 
         while(stk.size()!=0 && temperatures[stk.peek()] <= temperatures[i]){
             stk.pop();
