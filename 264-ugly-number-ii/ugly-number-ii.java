dad39857beb1005c2e2ea116f4a6bdf4b15c928e
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2025-07-02 19:41:08 +0530

    Time: 7 ms (43.88%) | Memory: 44.1 MB (44.28%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,70 +1,28 @@
 class Solution {
     public int nthUglyNumber(int n) {
 
-        if(n == 1) return 1;
-        List<Integer> uglyNumber = new ArrayList<>();
+        int num2=0;
+        int num3=0;
+        int num5=0;
 
-        int num2=0, num3 =0, num5=0;
+        List<Integer> uglyNo = new ArrayList<>();
+        uglyNo.add(1);
 
-        uglyNumber.add(1);
-        while(uglyNumber.size() < n){
-            int min = Math.min(uglyNumber.get(num2) * 2 ,Math.min(uglyNumber.get(num3) * 3 , uglyNumber.get(num5) * 5));
-            uglyNumber.add(min);    
-            if(min ==uglyNumber.get(num2) * 2){
+        while(uglyNo.size() < n){
+
+            int mn = Math.min(uglyNo.get(num2) * 2, Math.min(uglyNo.get(num3) * 3 , uglyNo.get(num5) * 5));
+            uglyNo.add(mn);
+            
+            if(mn == uglyNo.get(num2) * 2){
                 num2++;
-            }if(min ==uglyNumber.get(num3) * 3){
+            }
+            if(mn == uglyNo.get(num3) * 3){
                 num3++;
-            }if(min ==uglyNumber.get(num5) * 5){
+            }
+            if(mn == uglyNo.get(num5) * 5){
                 num5++;
             }
-        }
-        return uglyNumber.get(uglyNumber.size()-1);
-        
-
+        }   
+        return uglyNo.get(n-1);
     }
-    //     PriorityQueue<Integer> minH= new PriorityQueue<>(Collections.reverseOrder());
-    //     if(n == 1) return 1;
-    //     int count=0,num=2;
-    //     minH.add(1);
-    //     while(count!=n-1){
-
-    //         if(primeFactor(num)){
-    //             count++;
-    //             minH.add(num);
-    //         }
-    //         num++;
-    //     }
-    //     return minH.peek();
-
-
-    // }
-
-    // static boolean primeFactor(int n){
-
-    //     List<Integer> list= new ArrayList<>();
-
-      
-    //     while(n % 2 == 0){
-    //         list.add(2);
-    //         n/=2;
-    //     }
-
-    //     for(int i=3;i*i<=n;i+=2){
-    //         while(n % i == 0){
-    //             list.add(i);
-    //             n/=i;
-    //         }
-    //     }
-
-    //     if(n > 2){
-    //         list.add(n);
-    //     }
-    //     for(int i=0;i<list.size();i++){
-    //         if(list.get(i) > 5){
-    //              return false;
-    //         }
-    //     }
-    //     return true;
-
-    // }
 }
