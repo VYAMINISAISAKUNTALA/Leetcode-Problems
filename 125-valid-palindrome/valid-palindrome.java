5c8d73efafee43943d636d8974a287954e706d0d
VYAMINISAISAKUNTALA <47749518+VYAMINISAISAKUNTALA@users.noreply.github.com>
2026-01-15 18:13:04 +0530

    Time: 21 ms (9.56%) | Memory: 47.1 MB (13.77%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int len = s.length();
 
-        if(len == 1) return true;
-        StringBuilder str=new StringBuilder();
-        for(int i=0;i<len;i++){
-            if(s.charAt(i) >= 'A' && s.charAt(i) <='Z'){
-                str.append((char)(s.charAt(i) + ' '));
-            }else if(s.charAt(i) >='a' && s.charAt(i) <='z' || s.charAt(i) >='0' && s.charAt(i) <='9'){
-                str.append(s.charAt(i));
-            }
+
+        String str = s.toLowerCase();
+        str= str.replaceAll("[^a-zA-Z0-9]","");
+        System.out.println(str);
+        
+
+        int strLength=str.length();
+
+        
+        StringBuilder reverseStr = new StringBuilder();
+
+        for(int i=strLength-1;i>=0;i--){
+            reverseStr.append(str.charAt(i));
         }
-        int n = str.length();
-        StringBuilder revStr=new StringBuilder();
-        for(int i = n-1;i>=0;i--){
-            revStr.append(str.charAt(i));
-        }
-        return str.toString().equals(revStr.toString());
+
+        return str.contains(reverseStr.toString());
+
+
+
     }
 }
